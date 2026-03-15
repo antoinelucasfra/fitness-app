77f13c37e1d6c6311aecfbf406ab33a75a259dc3
antoinelucas <antoine.lucas.fra@gmail.com>
2026-03-15 16:52:49 +0100

    chores: remove files

--- run.R
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env Rscript
-# Run the fitness app
-# Usage: Rscript run.R
-#   or open in RStudio and click "Run App"
-
-renv::load()
-library(fitnessapp)
-fitnessapp::run_app(options = list(port = 3838, launch.browser = TRUE))
